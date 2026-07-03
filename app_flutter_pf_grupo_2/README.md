05f548bf1d7ece264f02689f6b013a3e46aef996
TizianoAugustoCastroPerez <49189896@est.ort.edu.ar>
2026-07-02 21:24:24 -0300

    primera versión del proyecto: códigos sueltos

---
+++ app_flutter_pf_grupo_2/README.md
@@ -0,0 +1,3 @@
+# app_flutter_pf_grupo_2
+
+A new Flutter project.
---
+++ app_flutter_pf_grupo_2/android/app/src/main/kotlin/com/example/app_flutter_pf_grupo_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_flutter_pf_grupo_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
